57288db42d21fec57fb4c88eafb5b1968890d23a
RbDmit <rbdmit@gmail.com>
2017-02-18 01:00:32 +0300

    singletone and state machine

---
+++ IStateMachine.cs
@@ -0,0 +1,33 @@
+﻿//using System.Collections;
+/* State interface */
+public delegate void ChangeState (ITransition transition);
+public interface IState {
+	event ChangeState OnExitState;
+	void Enter ();
+	void Exit  ();
+	/* metnod Update() calls from StateMachine in its Update() methon */
+	void Update();
+	void FixedUpdate ();
+}
+
+/* Transition interface */
+public delegate void TransitionFinishedHandler();
+public interface ITransition {
+	event TransitionFinishedHandler OnTransitionFinished;
+	/* metnod Update() calls from StateMachine in its Update() methon */
+	void Update();
+	void FixedUpdate ();
+	IState desireState { get; }
+}
+
+/* Transition abstract class */
+public abstract class Transition {
+	public Transition (IState state){ desireState = state; }
+	IState desireState;
+}
+
+/* State Machine interface */
+public interface IStateMachine {
+	// reciave events of exit state here
+	void ChangeState (ITransition transition);
+}
---
+++ Toolbox.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Toolbox : Singleton<Toolbox> {
+	protected Toolbox () {} // guarantee this will be always a singleton only - can't use the constructor!
+
+	public string myGlobalVar = "whatever";
+
+	public Language language = new Language();
+
+	public GameStateMachine gameStateMachine;
+
+	void Awake () {
+		// Your initialization code here
+	}
+
+	// (optional) allow runtime registration of global objects
+	static public T RegisterComponent<T> () where T: Component {
+		return Instance.GetOrAddComponent<T>();
+	}
+
+}
+
+[System.Serializable]
+public class Language {
+	public string current;
+	public string lastLang;
+}
